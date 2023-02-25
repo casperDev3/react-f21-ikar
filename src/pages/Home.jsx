9248efb3c1979be7505081ce43234b99ad3f58f2
zerodott <93039953+casperDev3@users.noreply.github.com>
2023-02-25 14:53:24 +0200

    add output array

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,27 +1,44 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 
 const Home = () => {
-  let [news, setNews] = useState([]); 
+  let [news, setNews] = useState([]);
 
   useEffect(() => {
     getNews();
   }, []);
 
   useEffect(() => {
-    console.log(news)
+    console.log(news);
   }, [news]);
 
-  const BASE_URL = 'https://admin.ikar-thinktank.org/api';
+  const BASE_URL = "https://admin.ikar-thinktank.org/api";
   const getNews = async () => {
     await fetch(`${BASE_URL}/news`)
-      .then(response => response.json())
-      .then(data => {
-        setNews(data);
-      })
+      .then((response) => response.json())
+      .then((res) => {
+        const CLEARED_DATA = res.data.map((item) => {
+          return {
+            id: item.id,
+            ...item.attributes,
+          };
+        });
+        setNews(CLEARED_DATA);
+      });
   };
-  return (
-    <div>Home</div>
-  )
-}
 
-export default Home
+  return (
+    <>
+      <div>Home</div>
+      {news.map((item) => {
+        return (
+          <div key={item.id}>
+            <p>{item.title}</p>
+            <p>{item.text}</p>
+          </div>
+        );
+      })}
+    </>
+  );
+};
+
+export default Home;
